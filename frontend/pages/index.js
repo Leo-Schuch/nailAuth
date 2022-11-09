8c6a4297ef0b24be22b1d0e52d1f340ae76540d6
Birulaibe Bola de Fogo <leoschuch23@gmail.com>
2022-11-09 08:16:59 -0300

    auth

---
+++ frontend/pages/index.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import { useRouter } from 'next/router';
+import { authService } from '../src/services/auth/authService';
+
+export default function HomeScreen() {
+  const router = useRouter();
+  const [values, setValues] = React.useState({
+    usuario: 'omariosouto',
+    senha: 'safepassword',
+  });
+
+  function handleChange(event) {
+    const fieldValue = event.target.value;
+    const fieldName = event.target.name;
+    setValues((currentValues) => {
+      return {
+        ...currentValues,
+        [fieldName]: fieldValue,
+      };
+    })
+  }
+
+  return (
+    <div>
+      <h1>Login</h1>
+      <form onSubmit={(event) => {
+        // onSubmit -> Controller (pega dados do usuário e passa pra um serviço)
+        // authService -> Serviço
+        event.preventDefault();
+        authService
+          .login({
+            username: values.usuario,
+            password: values.senha,
+          })
+          .then(() => {
+            router.push('/auth-page-static');
+            // router.push('/auth-page-ssr');
+          })
+          .catch((err) => {
+            console.log(err);
+            alert('Usuário ou a senha estão inválidos')
+          })
+      }}>
+        <input
+          placeholder="Usuário" name="usuario"
+          value={values.usuario} onChange={handleChange}
+        />
+        <input
+          placeholder="Senha" name="senha" type="password"
+          value={values.senha} onChange={handleChange}
+        />
+        {/* <pre>
+          {JSON.stringify(values, null, 2)}
+        </pre> */}
+        <div>
+          <button>
+            Entrar
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+}
